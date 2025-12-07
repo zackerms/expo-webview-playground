c6157f6c75e659f3c29199d1c9b4c33891944479
zacker <dev.zacker@gmail.com>
2025-12-07 11:43:56 +0900

    refactor: 不要な要素を削除

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
